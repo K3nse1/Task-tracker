210f8fb2d97e5af8c8dc803d3d2d7ba473da3048
Raúl Santos Gutiérrez <rsantosg@MacBook-Air-de-Raul.local>
2025-08-02 16:09:03 +0200

    Task-tracker v1.0

--- task-tracker.py
+++ task-tracker.py
@@ -1,17 +1,24 @@
 import sys
 import json
-import os
-import datetime
 
 def get_help():
-        print("TODO: Here we'll provide help with commands")
+        return '''
+        add: add a task to the list. Must specify a string between double quotes
 
+        delete: remove a task from the list. Specify the number of the task as an integer
+        
+        mark-in-progress: change the task status to in-progress. Specify the number of the task as an integer
+        
+        mark-done: change the task status to done. Specify the number of the task as an integer
+        
+        list_tasks: show the whole list of tasks
+'''
 class TaskTracker:
     
     def __init__(self):
         self.file_path = "tasks.json"
         self.file = None
-        self.valid_commands = ['add', 'update', 'delete', 'mark-in-progress', 'mark-done', 'list_tasks']
+        self.valid_commands = ['add', 'delete', 'mark-in-progress', 'mark-done', 'list-tasks']
 
     def open_file(self, mode:str):
         '''We don't need full permisions in the file to do every task. This method provides us the possibility to open the file in the right mode for each task'''
@@ -39,16 +46,12 @@ class TaskTracker:
         return self.file
 
     def update_task_numbers(self):
-        self.open_file('r+')
+        if self.file is None:
+            self.open_file('r+')
+        new_file = {}
         for idx, key in enumerate(list(self.file.keys())):
-            self.file[f"task{idx + 1}"] = self.file[key]
-            del self.file[key]
-        # idx = 0
-        # keys_list = list(self.file.keys())
-        # while idx < len(keys_list):
-        #     self.file[f"task{idx + 1}"] = self.file[keys_list[idx]]
-        #     del self.file[keys_list[idx]]
-        #     idx += 1
+            new_file[f"task{idx + 1}"] = self.file[key]
+        self.file = new_file
         return self.file
 
     def delete(self, id:int):
@@ -72,21 +75,35 @@ class TaskTracker:
 
     def list_tasks(self):
         self.open_file('r')
-        return self.file
+        print("\n📋 YOUR TASKS:")
+        print("=" * 60)
+        
+        for task_id, task in self.file.items():
+            id_num = task_id.replace('task', '')
+            status_emoji = "✅" if task['status'] == "Done" else "🔄" if task['status'] == "In progress" else "⏳"
+            
+            print(f"{status_emoji} [{id_num}] {task['title']} ({task['status']})")
+        
+        print("=" * 60)
     
     def main(self):
-        command = sys.argv[1]
 
+        try:
+            command = sys.argv[1]
+        except IndexError:
+            print(f'''Please, provide a valid command:\n{get_help()}''')
+            return
+        
         if command not in self.valid_commands:
-            f'''Wrong command. The app support these:
-            {get_help}'''
+            print(f'''Wrong command. The app support these:
+            {get_help()}''')
             return
 
         if len(sys.argv) == 1:
-             get_help()
+             print(get_help())
              return
 
-        if (len(sys.argv) == 2) and (command in self.valid_commands):
+        if (len(sys.argv) == 2) and (command in self.valid_commands) and (command != 'list-tasks'):
             print(f"TODO: Here we'll specify the arguments that the user must provide with that command")
             return
         
@@ -94,18 +111,14 @@ class TaskTracker:
             if command == 'add':
                 self.add(sys.argv[2])
             elif command == 'delete':
-                self.delete(sys.argv[2])
+                self.delete(int(sys.argv[2]))
             elif command == 'mark-in-progress':
                 self.mark_in_progress(sys.argv[2])
             elif command == 'mark-done':
                 self.mark_done(sys.argv[2])
-            elif command == 'list_tasks':
+            elif command == 'list-tasks':
                 self.list_tasks()
 
 if __name__ == '__main__':
     app = TaskTracker()
-    # app.main()
-    app.add('Hola')
-    app.add('Hola')
-    app.delete(2)
-    # app.add('Complete the add method')
+    app.main()
